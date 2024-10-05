53ebe0bce4ab4383958f4ad8216eb38665930b06
Lim <3269989624@qq.com>
2024-10-05 20:39:45 +0800

    :bookmark:

--- module01/src/com/collection/Demo02/Card.java
+++ module01/src/com/collection/Demo02/Card.java
@@ -1,17 +1,25 @@
 package com.collection.Demo02;
 
-public class Card {
-    private static final Card KING = new Card("\uD83E\uDD34","\uD83E\uDD34");
-    //"\uD83E\uDD34"
-    private static final Card QUEEN = new Card("\uD83D\uDC78","\uD83D\uDC78");
-    //"\uD83D\uDC78"
-    private String suit ;
-    //{"♣","♥","♦","♠"}
-    private String point;
-    //{"3","4","5","6","7","8","9","10","J","Q","K","A","2"};
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+
+public class Card implements Comparable<Card>{
+    private String suit ;
+    private String point;
+
+    //"\uD83E\uDD34"
+    private static final Card KING = new Card("\uD83E\uDD34","\uD83E\uDD34");
+    //"\uD83D\uDC78"
+    private static final Card QUEEN = new Card("\uD83D\uDC78","\uD83D\uDC78");
+    //{"♣","♥","♦","♠"}
+    public static final List<String> SUITS = new ArrayList<>(Arrays.asList("♣","♥","♦","♠"));
+    //{"3","4","5","6","7","8","9","10","J","Q","K","A","2"};
+    public static final List<String> POINTS = new ArrayList<>(Arrays.asList("3","4","5","6","7","8","9","10","J","Q","K","A","2","\uD83E\uDD34","\uD83D\uDC78"));
 
     public Card() {
+
     }
 
     public Card(String suit, String point) {
@@ -45,9 +53,26 @@ public class Card {
 
     @Override
     public String toString() {
-        return "Card{" +
-                "suit='" + suit + '\'' +
-                ", point='" + point + '\'' +
-                '}';
+        return suit+point;
+    }
+
+    @Override
+    public int compareTo(Card o) {
+        if(this.suit.equals("\uD83E\uDD34")){
+            return 1;
+        } else if ((this.suit.equals("\uD83D\uDC78"))) {
+            if((!o.suit.equals("\uD83E\uDD34"))){
+                return 1;
+            } else return -1;
+        } else if (POINTS.indexOf(this.point)>POINTS.indexOf(o.point)) {
+            return 1;
+
+        } else if (POINTS.indexOf(this.point)==POINTS.indexOf(o.point)) {
+            if(SUITS.indexOf(this.suit)>SUITS.indexOf(o.suit)){
+                return 1;
+            }else return -1;
+        } else if (POINTS.indexOf(this.point)<POINTS.indexOf(o.point)) {
+            return -1;
+        }else return 0;
     }
 }
--- module01/src/com/collection/Demo02/CardOperator.java
+++ module01/src/com/collection/Demo02/CardOperator.java
@@ -4,50 +4,85 @@ import java.util.*;
 
 public class CardOperator {
     private List<Card> cardList = new ArrayList<>();
-    private List<String> suits = new ArrayList<>();
-    //{"♣","♥","♦","♠"}
-    private List<String> points = new ArrayList<>();
-    //{"3","4","5","6","7","8","9","10","J","Q","K","A","2"};
+    public static List<Card> cardList1 = new ArrayList<>();
+    public static List<Card> cardList2 = new ArrayList<>();
+    public static List<Card> cardList3 = new ArrayList<>();
 
-    //准备54张牌
+    //0.准备54张牌
     public CardOperator() {
         cardList.add(Card.getKing());
         cardList.add(Card.getQueen());
-        Collections.addAll(suits, "♦", "♣", "♥", "♠");
-        Collections.addAll(points, "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A", "2");
-        Iterator<String> i1 = suits.iterator();
-        Iterator<String> i2 = points.iterator();
-        for (String suit : suits) {
+
+        Iterator<String> i1 = Card.SUITS.iterator();
+        Iterator<String> i2 = Card.POINTS.iterator();
+        for (int i = 0; i < Card.POINTS.size() - 2; i++) {
             for (int j = 0; j < 4; j++) {
-                cardList.add(new Card(suit, points.get(j)));
+                cardList.add(new Card(Card.SUITS.get(j), Card.POINTS.get(i)));
             }
         }
     }
 
-    public void showCardList() {
-        System.out.println(cardList);
-    }
 
-    //洗牌
+    //1.洗牌
     public void shuffle() {
         Collections.shuffle(cardList);
+        showCardList();
     }
 
-    //发牌
+    //2.发牌
     public void distributeCard() {
         for (int i = 0; i < cardList.size() - 3; i += 3) {
-            Player.p1.addCard(cardList.get(i));
-            Player.p2.addCard(cardList.get(i + 1));
-            Player.p3.addCard(cardList.get(i + 2));
+            cardList1.add(cardList.get(i));
+            cardList2.add(cardList.get(i + 1));
+            cardList3.add(cardList.get(i + 2));
         }
     }
 
-    //对牌顺序
+    //3.对牌顺序
     public void sortCard() {
-        Collections.sort(Player.p1.getCardList(), new Comparator<Card>() {
-            @Override
-            public int compare(Card o1, Card o2) {
-                if (suits.indexOf(o1.getSuit()) > suits.indexOf(o2.getSuit())) {
+        Collections.sort(cardList1);
+        Collections.sort(cardList2);
+        Collections.sort(cardList3);
+    }
+
+
+    //输出牌组信息
+    public void showCardList() {
+        System.out.println(cardList.get(0).getSuit() + cardList.get(0).getPoint());
+        System.out.println(cardList.get(1).getSuit() + cardList.get(1).getPoint());
+        int i = 0;
+        for (int j = 2; j < cardList.size(); j++) {
+            System.out.print(cardList.get(j).getSuit() + cardList.get(j).getPoint() + " ");
+            i++;
+            if (i % 4 == 0) {
+                System.out.println();
+                i = 0;
+            }
+        }
+    }
+
+    //底牌展示
+    public void showLastCard() {
+        System.out.println("总张数："+cardList.size());
+        System.out.println("P1张数："+cardList1.size());
+        System.out.println("P2张数："+cardList2.size());
+        System.out.println("P3张数："+cardList3.size());
+        System.out.println("P1+P2+P3张数:"+(cardList1.size()+cardList2.size()+cardList3.size()));
+        System.out.print("底牌：");
+        System.out.print(cardList.get(51).getSuit() + cardList.get(51).getPoint() + " ");
+        System.out.print(cardList.get(52).getSuit() + cardList.get(52).getPoint() + " ");
+        System.out.print(cardList.get(53).getSuit() + cardList.get(53).getPoint());
+    }
+
+
+
+
+
+
+
+
+    /*
+    if (suits.indexOf(o1.getSuit()) > suits.indexOf(o2.getSuit())) {
                     if (points.indexOf(o1.getPoint()) > points.indexOf(o2.getPoint())) {
                         return 1;
                     } else if (points.indexOf(o1.getPoint()) < points.indexOf(o2.getPoint())) {
@@ -56,11 +91,5 @@ public class CardOperator {
                 } else if (suits.indexOf(o1.getSuit()) < suits.indexOf(o2.getSuit())) {
                     return -1;
                 } else return 0;
-            }
-        });
-    }
-
-    /*//展示
-    public void display() {
-    }*/
+    */
 }
