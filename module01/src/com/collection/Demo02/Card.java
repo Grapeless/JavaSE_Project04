d3a47d8fafc624cec07eb787ba964569089f8a53
Lim <3269989624@qq.com>
2024-10-05 17:37:45 +0800

    :sparkles:24.10.5:sparkles:

--- module01/src/com/collection/Demo02/Card.java
+++ module01/src/com/collection/Demo02/Card.java
@@ -1,51 +1,53 @@
 package com.collection.Demo02;
 
 public class Card {
-    private static String KING = "\uD83E\uDD34";
-    private static String QUEEN = "\uD83D\uDC78";
-    private String[] suit ;
+    private static final Card KING = new Card("\uD83E\uDD34","\uD83E\uDD34");
+    //"\uD83E\uDD34"
+    private static final Card QUEEN = new Card("\uD83D\uDC78","\uD83D\uDC78");
+    //"\uD83D\uDC78"
+    private String suit ;
     //{"♣","♥","♦","♠"}
-    private String[] point;
+    private String point;
     //{"3","4","5","6","7","8","9","10","J","Q","K","A","2"};
 
 
     public Card() {
     }
 
-    public Card(String[] suit, String[] point) {
+    public Card(String suit, String point) {
         this.suit = suit;
         this.point = point;
     }
 
-    public static String getKING() {
-        return KING;
-    }
-
-    public static void setKING(String KING) {
-        Card.KING = KING;
-    }
-
-    public static String getQUEEN() {
-        return QUEEN;
-    }
-
-    public static void setQUEEN(String QUEEN) {
-        Card.QUEEN = QUEEN;
-    }
-
-    public String[] getSuit() {
+    public String getSuit() {
         return suit;
     }
 
-    public void setSuit(String[] suit) {
+    public void setSuit(String suit) {
         this.suit = suit;
     }
 
-    public String[] getPoint() {
+    public String getPoint() {
         return point;
     }
 
-    public void setPoint(String[] point) {
+    public void setPoint(String point) {
         this.point = point;
+    }
+
+    public static Card getKing(){
+        return KING;
+    }
+
+    public static Card getQueen(){
+        return QUEEN;
+    }
+
+    @Override
+    public String toString() {
+        return "Card{" +
+                "suit='" + suit + '\'' +
+                ", point='" + point + '\'' +
+                '}';
     }
 }
--- module01/src/com/collection/Demo02/Test.java
+++ module01/src/com/collection/Demo02/Test.java
@@ -3,4 +3,9 @@ package com.collection.Demo02;
 public class Test {
     //♣,♥,♦,♠
     //🤴,👸
+    public static void main(String[] args) {
+        CardOperator cardOperator = new CardOperator();
+        Player player = new Player();
+        player.showCardList();
+    }
 }
