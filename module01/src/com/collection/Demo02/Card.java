4731eefdcfe7f3f8ddd9ca44c156d9c48fa9949c
Lim <3269989624@qq.com>
2024-10-06 11:39:39 +0800

    :pencil2:

--- module01/src/com/collection/Demo02/Card.java
+++ module01/src/com/collection/Demo02/Card.java
@@ -5,18 +5,18 @@ import java.util.Arrays;
 import java.util.List;
 
 
-public class Card implements Comparable<Card>{
-    private String suit ;
+public class Card implements Comparable<Card> {
+    private String suit;
     private String point;
 
     //"\uD83E\uDD34"
-    private static final Card KING = new Card("\uD83E\uDD34","\uD83E\uDD34");
+    private static final Card KING = new Card("\uD83E\uDD34", "\uD83E\uDD34");
     //"\uD83D\uDC78"
-    private static final Card QUEEN = new Card("\uD83D\uDC78","\uD83D\uDC78");
+    private static final Card QUEEN = new Card("\uD83D\uDC78", "\uD83D\uDC78");
     //{"♣","♥","♦","♠"}
-    public static final List<String> SUITS = new ArrayList<>(Arrays.asList("♣","♥","♦","♠"));
+    public static final List<String> SUITS = new ArrayList<>(Arrays.asList("♦","♣", "♥", "♠"));
     //{"3","4","5","6","7","8","9","10","J","Q","K","A","2"};
-    public static final List<String> POINTS = new ArrayList<>(Arrays.asList("3","4","5","6","7","8","9","10","J","Q","K","A","2","\uD83E\uDD34","\uD83D\uDC78"));
+    public static final List<String> POINTS = new ArrayList<>(Arrays.asList("3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A", "2", "\uD83E\uDD34", "\uD83D\uDC78"));
 
     public Card() {
 
@@ -43,36 +43,36 @@ public class Card implements Comparable<Card>{
         this.point = point;
     }
 
-    public static Card getKing(){
+    public static Card getKing() {
         return KING;
     }
 
-    public static Card getQueen(){
+    public static Card getQueen() {
         return QUEEN;
     }
 
     @Override
     public String toString() {
-        return suit+point;
+        return suit + point;
     }
 
     @Override
     public int compareTo(Card o) {
-        if(this.suit.equals("\uD83E\uDD34")){
+        if (this.suit.equals("\uD83E\uDD34")) {
             return 1;
         } else if ((this.suit.equals("\uD83D\uDC78"))) {
-            if((!o.suit.equals("\uD83E\uDD34"))){
+            if ((!o.suit.equals("\uD83E\uDD34"))) {
                 return 1;
             } else return -1;
-        } else if (POINTS.indexOf(this.point)>POINTS.indexOf(o.point)) {
+        } else if (POINTS.indexOf(this.point) > POINTS.indexOf(o.point)) {
             return 1;
 
-        } else if (POINTS.indexOf(this.point)==POINTS.indexOf(o.point)) {
-            if(SUITS.indexOf(this.suit)>SUITS.indexOf(o.suit)){
+        } else if (POINTS.indexOf(this.point) == POINTS.indexOf(o.point)) {
+            if (SUITS.indexOf(this.suit) > SUITS.indexOf(o.suit)) {
                 return 1;
-            }else return -1;
-        } else if (POINTS.indexOf(this.point)<POINTS.indexOf(o.point)) {
+            } else return -1;
+        } else if (POINTS.indexOf(this.point) < POINTS.indexOf(o.point)) {
             return -1;
-        }else return 0;
+        } else return 0;
     }
 }
