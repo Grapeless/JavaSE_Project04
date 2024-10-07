125fdf8a24003fb9d321bba6f614ec1a3208d9a0
Lim <3269989624@qq.com>
2024-10-07 10:55:18 +0800

    :white_check_mark:

--- module01/src/com/map/HashMap/LinkedHashMap/Test.java
+++ module01/src/com/map/HashMap/LinkedHashMap/Test.java
@@ -1,4 +1,10 @@
 package com.map.HashMap.LinkedHashMap;
 
+import java.util.LinkedHashSet;
+
 public class Test {
+    //LinkedHashMap的底层原理同LinkedHashSet，LinkedHashSet即是一个不要值的LinkedHashMap
+    public static void main(String[] args) {
+        new LinkedHashSet<>();
+    }
 }
--- module01/src/com/map/HashMap/Test.java
+++ module01/src/com/map/HashMap/Test.java
@@ -1,4 +1,10 @@
 package com.map.HashMap;
 
+import java.util.HashSet;
+
 public class Test {
+    //HashMap底层实现原理同HashSet，HashSet即是一个不要值的HashMap
+    public static void main(String[] args) {
+        new HashSet<>();
+    }
 }
